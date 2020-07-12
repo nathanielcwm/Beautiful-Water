1a82f2612fbc8243cc430cc65105c0cec001c41f
Nathaniel Chin <22572406+nathanielcwm@users.noreply.github.com>
2020-07-12 13:31:29 +0800

    groundwork

---
+++ 1.1/Source/Beautiful-Water/Beautiful-Water/Class1.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+using HugsLib;
+using HugsLib.Settings;
+
+namespace Beautiful_Water
+{
+    public class WaterSettings : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get { return "BeautifulWater"; }
+        }
+        private SettingHandle WaterShallow;
+        public override void DefsLoaded()
+        {
+            WaterShallow = Settings.GetHandle("WaterShallow", "Shallow Water", "Min: -100, Max: 100", 1, Validators.IntRangeValidator(-100, 100));
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public class WaterMod
+    {
+        static WaterMod()
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
