698dd0d60e16068b406439500add4dfa6d60f05e
Nathaniel Chin <22572406+nathanielcwm@users.noreply.github.com>
2020-08-20 16:53:33 +0800

    Move files from 1.1 to 1.2

---
+++ 1.2/Source/Beautiful-Water/Beautiful-Water/BeautyConfig.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Beautiful_Water
+{
+    public class Settings : ModSettings
+    {
+        public float WaterShallow = 1.0f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look<float>(ref WaterShallow, "bwWaterShallow", 1f, false);
+            base.ExposeData();
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public class WaterMod : Mod
+    {
+        Settings settings;
+        static WaterMod()
+        {
+            Log.Message("Hello World!");
+        }
+        public WaterMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Settings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
+
+            listing_Standard.Label("Shallow Water Beauty");
+            settings.WaterShallow = listing_Standard.Slider(settings.WaterShallow, -50f, 50f);
+
+            listing_Standard.End();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Beautiful Water (Fork)";
+        }
+    }
+}
